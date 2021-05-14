fe2e2a35a4651c0ea31fe78c3728ce915290dc47
ayu350 <84146559+ayu350@users.noreply.github.com>
2021-05-14 10:29:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $("#img1").click(function(){
+    $("#flag").show();
+    $("#vedio").show();
+  });
+
+  $("#img1").click(function(){
+    $(this).hide();
+  });
+  });
+// --------------------------------------------------------------
+$(document).ready(function(){
+	$("#letter1").click(function(){
+		$(this).slideToggle();
+	});
+});
+
+$(document).ready(function(){
+	$("#letter2").click(function(){
+		$(this).slideToggle();
+	});
+});
+
+$(document).ready(function(){
+	$("#letter3").click(function(){
+		$(this).slideToggle();
+	});
+});
+// -------------------------------------------------------
+
+
+  
+
+
